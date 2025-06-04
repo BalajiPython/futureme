4ac354ad1dd83a9fca1ea9943faffe3753fa1e6f
BalajiPython <a21cadb08@sjctnc.edu.in>
2025-06-04 12:35:43 +0530

    Add new Render domain to ALLOWED_HOSTS

--- futureme/settings.py
+++ futureme/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'futureme.onrender.com').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'futureme.onrender.com,futureme-uwf5.onrender.com').split(',')
 
 # Application definition
 INSTALLED_APPS = [
